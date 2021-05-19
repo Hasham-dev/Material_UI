4d09b4e5fb61cffa8711b0c5d78da8a743807d6f
Hasham Vakani <vakanihasham64@gmail.com>
2021-05-20 01:51:01 +0500

    feat: adding design for grid & paper from material ui

--- src/Pages/Notes.jsx
+++ src/Pages/Notes.jsx
@@ -1,3 +1,4 @@
+import { Container, Grid, Paper } from '@material-ui/core'
 import React, { useEffect, useState } from 'react'
 import { db } from '../config/firebase'
 
@@ -22,12 +23,15 @@ function Notes () {
   }, [])
 
   return (
-    <div>
-      Notes
-      {notes?.map(note => (
-        <p key={note.id}>{note.title}</p>
-      ))}
-    </div>
+    <Container>
+      <Grid container>
+        {notes?.map(note => (
+          <Grid item xs={12} sm={6} lg={4} key={note.id}>
+            <Paper>{note.title}</Paper>
+          </Grid>
+        ))}
+      </Grid>
+    </Container>
   )
 }
 
---
+++ src/components/NoteCard.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function NoteCard ({ title, details }) {
+  return (
+    <div>
+      Note
+    </div>
+  )
+}
